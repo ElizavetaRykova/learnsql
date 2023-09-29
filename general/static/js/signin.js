3602108044aa1e5d3fe7923010d948e2f93ee10d
Elizaveta Rykova <elizaveta.rykova00@mail.ru>
2023-09-29 15:37:01 +0300

    Add frontend validation for login and password fields

---
+++ general/static/js/signin.js
@@ -0,0 +1,36 @@
+const loginForm = document.querySelector('.signin__form');
+loginForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  removeValidation();
+  checkFieldsPresence();
+})
+
+const generateError = function (text) {
+  const error = document.createElement('div');
+  error.classList.add('error');
+  error.style.color = '#B60000';
+  error.innerHTML = text;
+  return error;
+}
+
+const removeValidation = function () {
+  const errors = document.querySelectorAll('.error');
+  for (let i = 0; i < errors.length; i++) {
+    errors[i].remove()
+  }
+}
+
+const checkFieldsPresence = function () {
+  const login = document.querySelector('.input-login');
+  const password = document.querySelector('.input-password');
+  if (!login.value.length) {
+    console.log('field is blank', login);
+    const error = generateError('Введите логин');
+    login.parentElement.insertBefore(error, login);
+  }
+  if (!password.value.length) {
+    console.log('field is blank', password);
+    const error = generateError('Введите пароль');
+    password.parentElement.insertBefore(error, password);
+  }
+}
