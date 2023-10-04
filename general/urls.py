769b1445b440580e821a0a00b444dc7faf42d8e5
Elizaveta Rykova <elizaveta.rykova00@mail.ru>
2023-10-04 17:44:20 +0300

    Add HTML template for registration page

--- general/urls.py
+++ general/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.get_signin, name='get_signin'),
+    path('signup/', views.get_signup, name='signup'),
 ]
--- general/views.py
+++ general/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
-# Отрисовка HTML-шаблона страницы регистрации
+# Отрисовка HTML-шаблона страницы входа
 def get_signin(request):
     return render(request, 'signin.html')
+
+# Отрисовка HTML-шаблона страницы регистрации
+def get_signup(request):
+    return render(request, 'signup.html')
