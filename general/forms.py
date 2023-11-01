9dbefca16d3c2a39dce7df65eba6b9237778464e
Kashin Nikita <kashin.nik008@gmail.com>
2023-11-01 20:12:28 +0300

    Create registration form

--- general/forms.py
+++ general/forms.py
@@ -1,8 +1,9 @@
-from django.contrib.auth.forms import AuthenticationForm, UsernameField
+from django.contrib.auth.forms import AuthenticationForm, UsernameField, UserCreationForm
 from django import forms
+from django.contrib.auth.models import User
 
 
-class MyForm(AuthenticationForm):
+class MyAuthenticationForm(AuthenticationForm):
     username = UsernameField(widget=forms.TextInput(attrs={'autofocus': True, 
                                                            'placeholder': 'Логин',
                                                            'class': 'input-login'}))
@@ -13,3 +14,42 @@ class MyForm(AuthenticationForm):
                                           'placeholder': 'Пароль',
                                           'class': 'input-password'}),
     )
+
+
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'autofocus': True,
+                                                               'placeholder': 'Имя',
+                                                               'class': 'input-fio'}))
+    
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'autofocus': True,
+                                                               'placeholder': 'Фамилия',
+                                                               'class': 'input-fio'}))
+    
+    username = UsernameField(widget=forms.TextInput(attrs={'autofocus': True, 
+                                                           'placeholder': 'Логин',
+                                                           'class': 'input-login'}))
+    
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'autofocus': True, 
+                                                           'placeholder': 'Email',
+                                                           'class': 'input-email'}))
+
+    password1 = forms.CharField(
+        label= None,
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'current-password',
+                                          'placeholder': 'Пароль',
+                                          'class': 'input-password'}),
+    
+    )
+
+    password2 = forms.CharField(
+        label= None,
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'current-password',
+                                          'placeholder': 'Повторите пароль',
+                                          'class': 'input-password'}),
+    )
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2', )
--- general/views.py
+++ general/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 # from django.contrib.auth.forms import AuthenticationForm
-from .forms import MyForm
+from .forms import MyAuthenticationForm, RegisterForm
 
 # Форма входа
 def signin(request):
     if request.method == 'POST':
-        form = MyForm(request=request, data=request.POST)
+        form = MyAuthenticationForm(request=request, data=request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
@@ -17,13 +17,25 @@ def signin(request):
             else:
                 form.add_error(None, 'Invalid username or password')
     else:
-        form = MyForm()
+        form = MyAuthenticationForm()
     return render(request, './registration/login.html', {'form': form})
-
 
 # Отрисовка HTML-шаблона страницы регистрации
 def get_signup(request):
-    return render(request, 'signup.html')
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # получаем имя пользователя и пароль из формы
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            # выполняем аутентификацию
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('topic_list/')
+    else:
+        form = RegisterForm()
+    return render(request, 'signup.html', {'form': form})
 
 # Отрисовка HTML-шаблона страницы со списком тем
 def get_topic_list(request):
