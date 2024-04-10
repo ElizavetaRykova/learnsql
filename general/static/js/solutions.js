7f2a8546706873e5e9eac93b21706897567ba921
ElizavetaRykova <elizaveta.rykova00@mail.ru>
2024-04-10 21:56:31 +0300

    Add ability to comment student solution on frontend

--- general/static/js/solutions.js
+++ general/static/js/solutions.js
@@ -1,3 +1,4 @@
+//разворачивание ответа студента
 var acc = document.getElementsByClassName("solutions__item");
 var i;
 
@@ -9,6 +10,23 @@ for (i = 0; i < acc.length; i++) {
             panel.style.display = "none";
         } else {
             panel.style.display = "block";
+            var comment = document.querySelector('.solution__comment')
+            comment.style.display = "none";
+        }
+        //отображение блока для ввода комментариев
+        var button = document.getElementsByClassName("support-button");
+        var n;
+
+        for (n = 0; n < acc.length; n++) {
+            button[n].addEventListener("click", function() {
+                this.classList.toggle("active");
+                var comment = this.previousElementSibling;
+                if (comment.style.display === "block") {
+                    comment.style.display = "none";
+                } else {
+                    comment.style.display = "block";
+                }
+            });
         }
     });
 }
